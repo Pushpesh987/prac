4f410427a3dc2036a208e11fcc2e0ba9022c0302
Pushpesh987 <krpushpesh108@gmail.com>
2024-10-10 17:27:50 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
 
-import '../pages/currency_convertor_page.dart';
+// import '../pages/currency_convertor_page.dart';
+import 'pages/cupertino_design.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: CurrencyConvertorMaterialPage(),
+//     );
+//   }
+// }
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyCupertinoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyCupertinoApp extends StatelessWidget {
+  const MyCupertinoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CurrencyConvertorMaterialPage(),
+    return const CupertinoApp(
+      home: CurrencyConvertorCupertinoPage(),
     );
   }
 }
